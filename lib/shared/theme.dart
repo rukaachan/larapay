cbaf6cbb72ad971e1297711bf7c22921e376e1ce
rukaachan <novops911@gmail.com>
2024-08-15 19:25:29 +0700

    feat(theme): added colour palette

---
+++ lib/shared/theme.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+// 0xff = sign that we use hex number
+Color whiteColour = const Color(0xffFFFFFF);
+Color blackColor = const Color(0xff14193F);
+Color greyColour = const Color(0xffA4A8AE);
+Color lightBackgroundColour = const Color(0xffF6F8FB);
+Color darkBackgroundColour = const Color(0xff020518);
+Color blueColour = const Color(0xff53C1F9);
+Color purpleColour = const Color(0xff5142E6);
+Color greenColour = const Color(0xff22B07D);
+Color numberBackgroundColour = const Color(0xff1A1D2E);
