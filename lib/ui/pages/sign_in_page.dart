f2883739655cf0366526b36562284f3478508f61
rukaachan <novops911@gmail.com>
2024-10-12 07:57:17 +0700

    feat(pages): completed slicing sign in page

--- lib/ui/pages/sign_in_page.dart
+++ lib/ui/pages/sign_in_page.dart
@@ -33,9 +33,90 @@ class SignInPage extends StatelessWidget {
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(20), color: whiteColour),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 // EMAIL INPUT
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Email Address',
+                      style: blackTextStyle.copyWith(fontWeight: medium),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18)),
+                          contentPadding: const EdgeInsets.all(12)),
+                    )
+                  ],
+                ),
+                const SizedBox(height: 20),
+                // PASSWORD INPUT
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Password',
+                      style: blackTextStyle.copyWith(fontWeight: medium),
+                    ),
+                    const SizedBox(
+                      height: 8,
+                    ),
+                    TextFormField(
+                      obscureText: true, // Hide Password
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(18)),
+                          contentPadding: const EdgeInsets.all(12)),
+                    )
+                  ],
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                Align(
+                    alignment: Alignment.centerRight,
+                    child: Text(
+                      'Forgot Password?',
+                      style: blueTextStyle,
+                    )),
+                const SizedBox(
+                  height: 30,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: TextButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                        backgroundColor: purpleColour,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(56))),
+                    child: Text(
+                      'Sign In',
+                      style: whiteTextStyle.copyWith(
+                          fontSize: 16, fontWeight: semiBold),
+                    ),
+                  ),
+                ),
               ],
+            ),
+          ),
+          const SizedBox(height: 50),
+          SizedBox(
+            width: double.infinity,
+            height: 24,
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(padding: EdgeInsets.zero),
+              child: Text(
+                'Create New Account',
+                style: greyTextStyle.copyWith(fontSize: 18),
+              ),
             ),
           ),
         ],
