6645e0a3b3ff5f766feed6e83753e9e428afcf99
rukaachan <novops911@gmail.com>
2025-04-04 10:02:31 +0700

    feat(home): implement HomePage with bottom navigation bar

--- lib/ui/pages/home_page.dart
+++ lib/ui/pages/home_page.dart
@@ -1,10 +1,55 @@
 import "package:flutter/material.dart";
+import "package:larapay/shared/theme.dart";
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: lightBackgroundColour,
+
+      // List of items in the bottom navigation bar
+      bottomNavigationBar: BottomNavigationBar(
+        // Style of the bottom navigation bar
+        selectedItemColor: blueColour,
+        unselectedItemColor: blackColor,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        selectedLabelStyle:
+            blueTextStyle.copyWith(fontSize: 10, fontWeight: medium),
+        unselectedLabelStyle: blackTextStyle.copyWith(
+          fontSize: 10,
+          fontWeight: medium,
+        ),
+        items: [
+          BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_overview.png',
+                width: 20,
+                color: blueColour,
+              ),
+              label: 'Overview'),
+          BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_history.png',
+                width: 20,
+              ),
+              label: 'History'),
+          BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_statistic.png',
+                width: 20,
+              ),
+              label: 'Statistic'),
+          BottomNavigationBarItem(
+              icon: Image.asset(
+                'assets/ic_reward.png',
+                width: 20,
+              ),
+              label: 'Reward'),
+        ],
+      ),
+    );
   }
 }
